5618986c5ad4dfd55ba6987a7a40d5130ddb5f5c
kellyzhang <golive.future@gmail.com>
2023-11-13 13:22:12 -0500

    changed the contents of StapeList

--- components/common/StapeList.jsx
+++ components/common/StapeList.jsx
@@ -8,57 +8,76 @@ const StapeList = () => {
     <div className="row">
       <div className="col-md-6 col-xl-4">
         <div className="start__single__item column__space--secondary">
-          <h4>Integrity</h4>
+          <h4>Reliability</h4>
           <p className="neutral-bottom">
-          We operate with honesty and uphold the highest standards of moral and ethical principles. Every transaction on our platform is built on trust.
+            At CEEtokenized, reliability is fundamental. We operate with
+            unwavering transparency and adhere to the highest ethical standards.
+            Every transaction on our platform is built on reliability, ensuring
+            a secure and straightforward experience for our global community of
+            investors.
           </p>
         </div>
       </div>
       <div className="col-md-6 col-xl-4">
-        <div className="start__single__item column__space--secondary">          
+        <div className="start__single__item column__space--secondary">
           <h4>Innovation</h4>
           <p className="neutral-bottom">
-          The world is constantly evolving, and so are we. We&apos;re committed to pioneering new solutions, tools, and technologies that redefine the real estate experience.
+            As the real estate landscape evolves, so does CEEtokenized.
+            Committed to pioneering user-friendly solutions, tools, and
+            technologies, we enhance the real estate experience through
+            tokenization. Join us on the forefront as we lead the future of
+            investment across Central Europe.
           </p>
         </div>
       </div>
       <div className="col-md-6 col-xl-4">
         <div className="start__single__item">
-          
-          <h4>Customer-Centricity</h4>
+          <h4>Customer-Focused Approach</h4>
           <p className="neutral-bottom">
-          Our users are at the heart of everything we do. We strive to understand their needs and provide tailored solutions that enhance their journey in the world of real estate.
+            At CEEtokenized, our customers are at the center of our mission. We
+            deeply understand their diverse needs, providing tailored solutions
+            that improve their journey in the world of tokenized real estate.
+            Your satisfaction drives our commitment to excellence in the global
+            real estate marketplace.
           </p>
         </div>
       </div>
       <div className="col-md-6 col-xl-4">
         <div className="start__single__item">
-          
           <h4>Collaboration</h4>
           <p className="neutral-bottom">
-            We believe in the power of teamwork. By fostering a culture of collaboration, we bring together diverse perspectives to create the best outcomes for our community.
+            We value collaborative efforts by fostering a culture of teamwork,
+            we unite diverse perspectives to achieve the best outcomes for our
+            global community of investors. Together, we fortify a resilient and
+            interconnected tokenized real estate ecosystem in Central Europe.
           </p>
         </div>
       </div>
       <div className="col-md-6 col-xl-4">
         <div className="start__single__item">
-          
           <h4>Continuous Learning</h4>
           <p className="neutral-bottom">
-          In the ever-changing landscape of real estate, we recognize the importance of growth and adaptability. We are committed to fostering an environment of continuous learning, equipping our team and users with the knowledge and resources they need to stay ahead in the industry.
+            In the dynamic landscape of tokenized real estate, continuous
+            learning is integral at CEEtokenized. Recognizing the importance of
+            growth and adaptability, we cultivate an environment of ongoing
+            learning. Engage with us as we empower our team and users to
+            navigate the ever-evolving terrain of Central European real estate
+            investments.
           </p>
         </div>
       </div>
       <div className="col-md-6 col-xl-4">
         <div className="start__single__item">
-          
-          <h4>Sustainability</h4>
+          <h4>Sustainable Impact</h4>
           <p className="neutral-bottom">
-          Our responsibility extends beyond business. We&apos;re dedicated to promoting sustainable practices, ensuring that our operations have a positive impact on the environment and communities we serve.
+            Our commitment extends beyond business. We are dedicated to
+            promoting sustainable practices in tokenized real estate, ensuring
+            that our operations foster positive change in the environment and
+            communities we serve. Join us in shaping a sustainable future for
+            global real estate investments in Central Europe.
           </p>
         </div>
       </div>
-
     </div>
   );
 };
